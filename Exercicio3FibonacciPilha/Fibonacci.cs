14935c73bf7b3664a6584f4166806a3bf605d809
gabsoares <soaresgabriel68@gmail.com>
2024-05-04 13:34:08 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio3FibonacciPilha/Fibonacci.cs
@@ -0,0 +1,28 @@
+﻿namespace Exercicio3FibonacciPilha
+{
+    internal class Fibonacci
+    {
+        int valor;
+        Fibonacci anterior;
+
+        public Fibonacci(int valor)
+        {
+            this.valor = valor;
+            this.anterior = null;
+        }
+        public void setAnterior(Fibonacci anterior)
+        {
+            this.anterior = anterior;
+        }
+
+        public Fibonacci getAnterior()
+        {
+            return this.anterior;
+        }
+
+        public override string? ToString()
+        {
+            return $"{this.valor}";
+        }
+    }
+}
---
+++ Exercicio3FibonacciPilha/PilhaFibonacci.cs
@@ -0,0 +1,43 @@
+﻿namespace Exercicio3FibonacciPilha
+{
+    internal class PilhaFibonacci
+    {
+        Fibonacci topo;
+
+        public PilhaFibonacci()
+        {
+            this.topo = null;
+        }
+
+        public void Push(Fibonacci aux)
+        {
+            if (Vazia())
+            {
+                this.topo = aux;
+            }
+            else
+            {
+                aux.setAnterior(topo);
+                topo = aux;
+            }
+        }
+
+        bool Vazia()
+        {
+            return topo == null;
+        }
+
+        public void RunOver()
+        {
+            Fibonacci aux = topo;
+            if (!Vazia())
+            {
+                do
+                {
+                    Console.WriteLine(aux.ToString());
+                    aux = aux.getAnterior();
+                } while (aux != null);
+            }
+        }
+    }
+}
